ff48d8bc9ded05f375e1f817e7a5eee1bad7242c
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-01-10 11:18:14 -0800

    fixup! Add Intel(R) Extension for Scikit-learn* example
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- scikit-learn-intelex/scripts/download_dataset.py
+++ scikit-learn-intelex/scripts/download_dataset.py
@@ -6,8 +6,8 @@ from sklearn.datasets import fetch_openml
 
 def main():
     X, y = fetch_openml(name='mnist_784', version=1, return_X_y=True, data_home='data', parser='auto')
-    X.to_csv('data/X.csv', index=False)
-    y.to_csv('data/y.csv', index=False)
+    X.to_csv('data/X.csv', index=False, header=False)
+    y.to_csv('data/y.csv', index=False, header=False)
 
 if __name__ == '__main__':
     main()
