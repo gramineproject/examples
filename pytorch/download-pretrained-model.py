2e41bcd1d1887a364fced4bd220e654c9ae41ff0
jkr0103 <jitender.kumar@intel.com>
2023-02-22 09:31:00 +0000

    Fix deprecation warning in PyTorch example
    Signed-off-by: jkr0103 <jitender.kumar@intel.com>

--- pytorch/download-pretrained-model.py
+++ pytorch/download-pretrained-model.py
@@ -2,7 +2,7 @@ from torchvision import models
 import torch
 
 output_filename = "alexnet-pretrained.pt"
-alexnet = models.alexnet(pretrained=True)
+alexnet = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
 torch.save(alexnet, output_filename)
 
 print("Pre-trained model was saved in \"%s\"" % output_filename)
