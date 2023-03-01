6a3fe91d02065b75e9fe2ce5b026daaf02d1b99b
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2023-03-01 01:31:14 -0800

    Allow new- and old-style download of models in PyTorch example
    Previous commit "Fix deprecation warning in PyTorch example" broke the
    PyTorch example on Ubuntu 18.04, CentOS 8, RHEL 8, etc. Changes in that
    commit work with torchvision 0.13.0+ which is available with Python 3.7+
    only, however e.g. Ubuntu 18.04 comes with Python 3.6, hence torchvision
    0.13.0+ cannot be installed on Ubuntu 18.04. To allow such older OS
    distros, this commit allows both styles of downloading models.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- pytorch/download-pretrained-model.py
+++ pytorch/download-pretrained-model.py
@@ -2,7 +2,13 @@ from torchvision import models
 import torch
 
 output_filename = "alexnet-pretrained.pt"
-alexnet = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
+
+try:
+    alexnet = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
+except AttributeError:
+    # older versions of torchvision (less than 0.13.0) use below now-deprecated parameter
+    alexnet = models.alexnet(pretrained=True)
+
 torch.save(alexnet, output_filename)
 
 print("Pre-trained model was saved in \"%s\"" % output_filename)
