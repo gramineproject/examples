3cbea559501a1b6145563a113e1daac5e9c71ff2
Michał Kowalczyk <mkow@invisiblethingslab.com>
2023-04-25 14:32:56 +0200

    Add missing license notices
    Seems we forgot to migrate the license notices when moving these
    examples out of the main repo.
    
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- r/scripts/sample.r
+++ r/scripts/sample.r
@@ -1,3 +1,5 @@
+# Copied from https://www.rexamples.com/14/Sample()
+
 print(sample(1:3))
 print(sample(1:3, size=3, replace=FALSE))  # same as previous line
 print(sample(c(2,5,3), size=4, replace=TRUE))
