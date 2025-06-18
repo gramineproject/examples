2aaee1bb743fd83329ec7e976d731e1b2a5263ac
Sonali <sonali.saha@intel.com>
2025-06-17 21:50:09 -0700

    fixup! pytorch: Set weights_only=False when loading models from a trusted source
    Signed-off-by: Sonali <sonali.saha@intel.com>

--- pytorch/pytorchexample.py
+++ pytorch/pytorchexample.py
@@ -4,11 +4,10 @@
 from torchvision import models
 import torch
 
-# Load the model from a file. This file needs to be obtained from a trusted
-# source, because it can contain code, not only data.
-# Note: For PyTorch 2.6 and later, unpickling with weights_only=True
-# (which was made the default) can fail if the model file contains more than
-# just the weights and includes classes or functions that are not allowlisted.
+# Load the model from a file. This file needs to be obtained from a trusted source, because it can
+# contain code, not only data. Note: For PyTorch 2.6 and later, unpickling with weights_only=True
+# (which was made the default) can fail if the model file contains more than just the weights and
+# includes classes or functions that are not allowlisted.
 alexnet = torch.load("alexnet-pretrained.pt", weights_only=False)
 
 # Prepare a transform to get the input image into a format (e.g., x,y dimensions) the classifier
